22779983349d1e24b665b92b20556f52852b70fd
mugundhan27 <mugundhan6313@gmail.com>
2026-01-04 20:08:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
     const navbar = document.getElementById("navbar");
 
-    if (window.scrollY > 50) {
+    if (window.scrollY > 60) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
